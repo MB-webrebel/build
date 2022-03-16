83ba49e6b92891bb8737ed4152449e620b66d576
Miroslav Baláž <47142224+MB-webrebel@users.noreply.github.com>
2022-03-16 08:49:51 +0100

    Update main.js

--- src/js/main.js
+++ src/js/main.js
@@ -1,12 +1,55 @@
-var button = document.querySelector('#button');
-var modal = document.querySelector('#modal');
-var close = document.querySelector('#close');
+$(document).ready(function(){
 
 
-button.addEventListener('click', function(){
-    modal.classList.add('modal_active');
-});
+    // Napojení knihovny wow.js
+    new WOW().init();
 
-close.addEventListener('click', function(){
-    modal.classList.remove('modal_active');
+    var button = $('#button');
+    var modal = $('#modal');
+    var close = $('#close');
+
+    button.on('click', function(){
+        modal.addClass('modal_active');
+    });
+
+    close.on('click', function(){
+        modal.removeClass('modal_active');
+    });
+
+
+    // Validace formuláře
+
+    $('#brief-form').validate({
+      rules: {
+        username: "required",
+        phone: "required",
+      },
+      messages: {
+        username: "Uveďte vaše jméno",
+        phone: "Uveďte váš telefon"
+      }
+    });
+
+
+    // Napojení slideru
+    $('.slider').slick({
+        slidesToShow: 3,
+        prevArrow: $('.arrows__left'),
+        nextArrow: $('.arrows__right'),
+        responsive: [
+            {
+              breakpoint: 1200,
+              settings: {
+                slidesToShow: 2,
+              }
+            },
+            {
+                breakpoint: 768,
+                settings: {
+                  slidesToShow: 1,
+                }
+              }
+        ]
+    });
+
 });
